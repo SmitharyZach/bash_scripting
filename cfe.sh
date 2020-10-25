ae2b1a2bb5f0da8140c4c776a0072689f20a395a
Zach Smith <znathanielsmith@gmail.com>
2020-10-25 16:55:13 -0400

    updates cfe script

--- cfe.sh
+++ cfe.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# this bash script will take an argument that is the name of the repository
+# the repository will create a skeleton bare bones front end files
+# the repo will then be initliazed via git
+# the files will be added and commited with message "First commit"
 
 
 mkdir $1
@@ -12,4 +16,8 @@ touch styles.css
 cd ..
 cd js
 touch scripts.js 
+cd ..
+git init
+git add .
+git commit -m "First commit"
 
